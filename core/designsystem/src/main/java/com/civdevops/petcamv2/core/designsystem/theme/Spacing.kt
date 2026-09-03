242a4ed5598b066605c98d51aa192bf1b606047e
Chathura Madushanka <jrcmadushanka@gmail.com>
2026-09-04 05:19:20 +0530

    Base theme implementation

---
+++ core/designsystem/src/main/java/com/civdevops/petcamv2/core/designsystem/theme/Spacing.kt
@@ -0,0 +1,15 @@
+package com.civdevops.petcamv2.core.designsystem.theme
+
+import androidx.compose.ui.unit.dp
+
+@Suppress("unused")
+object PetCamSpacing {
+    val none = 0.dp
+    val extraSmall = 4.dp
+    val small = 8.dp
+    val medium = 12.dp
+    val large = 16.dp
+    val extraLarge = 24.dp
+    val huge = 32.dp
+    val massive = 48.dp
+}
